c8dffab1bcb8c8de75b4ff116e673fd47519aa57
Bodhit <dhvandarji@gmail.com>
2024-01-17 19:27:16 +0530

    final

--- backend/routes/login.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const {loginController} = require("../controllers/LoginController");
-router.route("/login/:user").post(loginController);
-
-module.exports = router;

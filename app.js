4ae8f9bbce20305dc4825b25364b342ba0251d48
Bodhit <dhvandarji@gmail.com>
2024-01-17 21:18:28 +0530

    new updated code for server.js

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const teacherRoutes = require("./routes/teacher");
 const authRoute = require("./routes/login");
 const cors = require("cors");
 
+const pathname = path.resolve(__dirname, "/build/index.html");
+console.log(pathname);
+
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -18,11 +21,8 @@ app.use("/api", authRoute);
 
 app.use(express.static(path.join(__dirname, "build")));
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "/build/index.html"));
 });
 
 module.exports = app;
-
-const pathname = path.resolve(__dirname, "/build/index.html");
-console.log(pathname);
